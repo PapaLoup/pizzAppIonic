60793d5f733671079bf4b935797fd78cd0002ba4
Thibault <tleloup.tl@gmail.com>
2018-10-03 10:31:23 +0200

    toogle bar usefull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav,Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  pages: Array<{title: string, component: any}>;
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage }
+    ];
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +27,9 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { MaBellePage } from '../pages/ma-belle/ma-belle';
 import { PipesModule } from '../pipes/pipes.module';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 @NgModule({
@@ -15,7 +19,7 @@ import { PipesModule } from '../pipes/pipes.module';
     MyApp,
     HomePage,
     MaBellePage,
-
+    ListPage
   ],
   imports: [
     BrowserModule,
@@ -27,6 +31,7 @@ import { PipesModule } from '../pipes/pipes.module';
     MyApp,
     HomePage,
     MaBellePage,
+    ListPage
   ],
   providers: [
     StatusBar,
